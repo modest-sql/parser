55ee5165fb8016705ed911adc135d273c309e768
astrothing <andres.rodriguezhn@gmail.com>
2017-10-22 23:40:16 -0600

    Improved .gitignore

---
+++ samples/tokens.sql
@@ -0,0 +1,33 @@
+-- Expected tokens: 62
+
+-- Binary operators
++ - * / < > <= >= = between LIKE IS AND OR 
+
+-- Literals
+27819 3.1416 'Hello' NULL TRUE FALSE
+
+'Multi line
+string! Why not?'
+
+/*
+    Slash star comment
+*/
+
+-- Keywords
+SELECT FROM INNER JOIN ON WHERE GROUP BY NOT IN HAVING AS INTO SET
+
+SUM COUNT AVG min max
+
+CREATE ALTER DROP Insert UpDAte delete
+
+INTEGER FLOAT BOOLEAN CHAR DATETIME
+
+DEFAULT AUTO_INCREMENT
+
+-- Identifiers
+movies
+MOVIES.title
+movies2
+
+-- Misc
+() , ;
