845eefad156adee6540586a25519c65b11d67865
astrothing <andres.rodriguezhn@gmail.com>
2017-10-23 00:16:12 -0600

    Updated lexer test

--- lexer_test.go
+++ lexer_test.go
@@ -13,7 +13,6 @@ const (
 	KW_ON       = 0
 	KW_GROUP    = 0
 	KW_BY       = 0
-	KW_AS       = 0
 	KW_FLOAT    = 0
 	KW_BOOLEAN  = 0
 	KW_DATETIME = 0
@@ -35,7 +34,7 @@ func TestLexer(t *testing.T) {
 	lval := &yySymType{}
 
 	tokenCount := 0
-	expectedTokenCount := 62
+	expectedTokenCount := 64
 
 	var integers []int
 	var floats []float64
@@ -54,7 +53,7 @@ func TestLexer(t *testing.T) {
 			strings = append(strings, lval.string_t)
 		} else if token == TK_ID {
 			identifiers = append(identifiers, lval.string_t)
-		} else if token >= KW_OR && token <= KW_FALSE {
+		} else if token >= KW_OR && token <= KW_COLUMN {
 			keywords = append(keywords, token)
 		}
 
@@ -112,8 +111,8 @@ func TestLexer(t *testing.T) {
 	})
 
 	t.Run("TestIdentifiers", func(t *testing.T) {
-		expectedIdentifierCount := 4
 		expectedIdentifiers := []string{"movies", "movies", "title", "movies2"}
+		expectedIdentifierCount := len(expectedIdentifiers)
 		identifierCount := len(identifiers)
 
 		if identifierCount != expectedIdentifierCount {
@@ -169,6 +168,8 @@ func TestLexer(t *testing.T) {
 			KW_DATETIME,
 			KW_DEFAULT,
 			KW_AUTO_INCREMENT,
+			KW_COLUMN,
+			KW_ADD,
 		}
 
 		expectedKeywordCount := len(expectedKeywords)
